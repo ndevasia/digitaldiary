8abf1b4b3d5e6d48db52f3c4cbe685edecd28a3a
sophiaat <sophiaat@uw.edu>
2025-01-29 15:20:46 -0800

    CHANGE layout overlay.py

--- lib/overlay.py
+++ lib/overlay.py
@@ -2,16 +2,16 @@ import os
 import sys
 from datetime import datetime
 import threading
+import requests
 from flask import Flask, render_template  # Assuming you have a Flask app that serves a page
 import subprocess
-
+from lib.globals import USERNAME
 
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QFrame
 from PyQt5.QtCore import Qt, QDateTime
 from PyQt5.QtGui import QPixmap, QIcon
 from .recording import RecorderThread
 from .audio import AudioRecorderThread
-from .window import OutputWindow
 from lib.server.aws import S3
 
 # Ensure directories exist
@@ -42,7 +42,6 @@ class TransparentOverlay(QMainWindow):
         self.audioRecorderThread.stopped.connect(self.onAudioRecordingStopped)
         self.audioRecorderThread.started.connect(self.onAudioStarted)
         self.isAudioRecording = False
-        self.outputWindow = OutputWindow()
         s3_client_instance = S3()
         self.client = s3_client_instance.get()
 
@@ -53,50 +52,29 @@ class TransparentOverlay(QMainWindow):
         self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint | Qt.Tool)
         self.setAttribute(Qt.WA_TranslucentBackground, True)
 
-        # Set window size to cover the right side of the screen
+        # Set window size to cover more of the right side of the screen
         screen = QApplication.primaryScreen()
         screenGeometry = screen.geometry()
-        self.setGeometry(int(screenGeometry.width() - 300), int(screenGeometry.height() / 4), 300, int(screenGeometry.height() / 2)
-)
-
+        self.setGeometry(
+            screenGeometry.width() - 300,
+            int(screenGeometry.height() / 6),
+            300,
+            int(screenGeometry.height() * 2 / 3)
+        )
 
         # Create a central widget with a transparent background
         self.centralWidget = QWidget(self)
         self.centralWidget.setStyleSheet("background-color: rgba(240, 240, 240, 50);")
         self.setCentralWidget(self.centralWidget)
 
-        # Create a layout and set it for the central widget
+        # Set up a vertical layout to hold all buttons evenly spaced
         self.layout = QVBoxLayout(self.centralWidget)
-        self.layout.setContentsMargins(0, 0, 0, 0)
-        self.layout.setAlignment(Qt.AlignTop)
-
-        buttonFrame = QFrame(self.centralWidget)
-        buttonFrame.setFixedSize(200, 200)
-        buttonFrame.setStyleSheet("background:transparent;")
-        buttonLayout = QVBoxLayout(buttonFrame)
-        buttonLayout.setContentsMargins(0, 0, 0, 0)
-        buttonLayout.setAlignment(Qt.AlignTop)
-
-        # Create the screenshot button
-        self.screenshotButton = QPushButton(self)
-        self.screenshotButton.setFixedSize(200, 200)  # Set fixed size for the button
-        self.screenshotButton.setIcon(QIcon(resource_path('../icons/screenshot.svg')))
-        self.screenshotButton.setIconSize(self.screenshotButton.size())
-        self.screenshotButton.setStyleSheet("""
-                    QPushButton {
-                        background-color: lightgray;
-                        border-radius: 100px;
-                    }
-                    QPushButton:hover {
-                        background-color: gray;
-                    }
-                """)
-        self.screenshotButton.move(50, 800)
-        self.screenshotButton.clicked.connect(self.takeScreenshot)
+        self.layout.setContentsMargins(20, 20, 20, 20)
+        self.layout.setSpacing(20)
 
         # Create the kill switch button
-        self.killSwitchButton = QPushButton('X', self)
-        self.killSwitchButton.setFixedSize(50, 50)  # Set fixed size for the button
+        self.killSwitchButton = QPushButton('X', self.centralWidget)
+        self.killSwitchButton.setFixedSize(50, 50)
         self.killSwitchButton.setStyleSheet("""
                     QPushButton {
                         background-color: rgb(255, 0, 0);
@@ -109,39 +87,66 @@ class TransparentOverlay(QMainWindow):
                 """)
         self.killSwitchButton.clicked.connect(self.closeApplication)
 
-        self.recordButton = QPushButton(self)
-        self.recordButton.setFixedSize(200, 200)
+        # Add kill switch button to the layout
+        self.layout.addWidget(self.killSwitchButton, alignment=Qt.AlignCenter)
+
+        # Create the record button
+        self.recordButton = QPushButton(self.centralWidget)
+        self.recordButton.setFixedSize(100, 100)
         self.recordButton.setIcon(QIcon(resource_path('../icons/record-start.svg')))
         self.recordButton.setIconSize(self.recordButton.size())
         self.recordButton.setStyleSheet("""
                     QPushButton {
                         background-color: lightgray;
-                        border-radius: 100px;
-                        font-size: 24px;
+                        border-radius: 50px;
                     }
                     QPushButton:hover {
                         background-color: gray;
                     }
                 """)
-        self.recordButton.move(50, 400)
         self.recordButton.clicked.connect(self.toggleRecording)
 
-        self.audioButton = QPushButton(self)
-        self.audioButton.setFixedSize(200,200)
+        # Add record button to the layout
+        self.layout.addWidget(self.recordButton, alignment=Qt.AlignCenter)
+
+        # Create the audio button
+        self.audioButton = QPushButton(self.centralWidget)
+        self.audioButton.setFixedSize(100, 100)
         self.audioButton.setIcon(QIcon(resource_path('../icons/audio-start.svg')))
         self.audioButton.setIconSize(self.audioButton.size())
         self.audioButton.setStyleSheet("""
-                            QPushButton {
-                                background-color: lightgray;
-                                border-radius: 100px;
-                                font-size: 24px;
-                            }
-                            QPushButton:hover {
-                                background-color: gray;
-                            }
-                        """)
-        self.audioButton.move(50, 0)
+                    QPushButton {
+                        background-color: lightgray;
+                        border-radius: 50px;
+                    }
+                    QPushButton:hover {
+                        background-color: gray;
+                    }
+                """)
         self.audioButton.clicked.connect(self.toggleAudio)
+
+        # Add audio button to the layout
+        self.layout.addWidget(self.audioButton, alignment=Qt.AlignCenter)
+
+        # Create the screenshot button
+        self.screenshotButton = QPushButton(self.centralWidget)
+        self.screenshotButton.setFixedSize(100, 100)
+        self.screenshotButton.setIcon(QIcon(resource_path('../icons/screenshot.svg')))
+        self.screenshotButton.setIconSize(self.screenshotButton.size())
+        self.screenshotButton.setStyleSheet("""
+                    QPushButton {
+                        background-color: lightgray;
+                        border-radius: 50px;
+                    }
+                    QPushButton:hover {
+                        background-color: gray;
+                    }
+                """)
+        self.screenshotButton.clicked.connect(self.takeScreenshot)
+
+        # Add screenshot button to the layout
+        self.layout.addWidget(self.screenshotButton, alignment=Qt.AlignCenter)
+
 
     def takeScreenshot(self):
         # Generate a filename with date and time
@@ -149,18 +154,26 @@ class TransparentOverlay(QMainWindow):
         screenshotPath = os.path.abspath(f'../screenshots/screenshot_{now}.png')
 
         # Take the screenshot and save it
+        print("Taking screenshot")
         screenshot = QApplication.primaryScreen().grabWindow(0)
         screenshot.save(screenshotPath, 'png')
 
-        # Upload to S3
-        self.client.upload(screenshotPath)
+        screenshot_url = self.client.get_presigned_url(screenshotPath)
+        print(f"Screenshot URL: {screenshot_url}")  # Debugging: check if the URL is correct
+
+        try:
+            with open(screenshotPath, 'rb') as f:
+                response = requests.put(screenshot_url, data=f)
+                if response.status_code == 200:
+                    print("Screenshot uploaded successfully.")
+                else:
+                    print(f"Failed to upload screenshot. Status Code: {response.status_code}, Response: {response.text}")
+        except Exception as e:
+            print(f"Error uploading screenshot: {str(e)}")
 
         # Start Flask app in a separate thread
-        flask_thread = threading.Thread(target=run_flask_app, daemon=True)
-        flask_thread.start()
-
-        print(f"Screenshot saved at {screenshotPath} and Flask app started!")
-
+        # flask_thread = threading.Thread(target=run_flask_app, daemon=True)
+        # flask_thread.start()
 
     def toggleRecording(self):
         if self.isRecording:
@@ -182,17 +195,49 @@ class TransparentOverlay(QMainWindow):
 
     def onRecordingStopped(self):
         self.recordButton.setIcon(QIcon(resource_path('../icons/record-start.svg')))
-        self.client.upload(self.recorderThread.video_path)
-        self.client.upload(self.recorderThread.thumbnail_path)
-        self.outputWindow.show()
-        self.outputWindow.showVideo(self.recorderThread.thumbnail_path)
+        # Generate pre-signed URL for the video file
+        video_url = self.client.get_presigned_url(self.recorderThread.video_path)
+        print(f"Video URL: {video_url}")  # Debugging: check if the URL is correct
+
+        try:
+            with open(self.recorderThread.video_path, 'rb') as f:
+                response = requests.put(video_url, data=f)
+                if response.status_code == 200:
+                    print("Video uploaded successfully.")
+                else:
+                    print(f"Failed to upload video. Status Code: {response.status_code}, Response: {response.text}")
+        except Exception as e:
+            print(f"Error uploading video: {str(e)}")
+
+        # Generate pre-signed URL for the thumbnail file
+        thumbnail_url = self.client.get_presigned_url(self.recorderThread.thumbnail_path)
+        print(f"Thumbnail URL: {thumbnail_url}")  # Debugging: check if the URL is correct
+
+        try:
+            with open(self.recorderThread.thumbnail_path, 'rb') as f:
+                response = requests.put(thumbnail_url, data=f)
+                if response.status_code == 200:
+                    print("Thumbnail uploaded successfully.")
+                else:
+                    print(f"Failed to upload thumbnail. Status Code: {response.status_code}, Response: {response.text}")
+        except Exception as e:
+            print(f"Error uploading thumbnail: {str(e)}")
 
     def onAudioRecordingStopped(self):
         self.audioButton.setIcon(QIcon(resource_path('../icons/audio-start.svg')))
-        self.client.upload(self.audioRecorderThread.audio_path)
-        self.outputWindow.show()
-        self.outputWindow.showAudio(self.audioRecorderThread.audio_path)
+        # Generate pre-signed URL
+        audio_url = self.client.get_presigned_url(self.audioRecorderThread.audio_path)
+        print(f"Audio URL: {audio_url}")  # Debugging: check if the URL is correct
 
+        try:
+            with open(self.audioRecorderThread.audio_path, 'rb') as f:
+                response = requests.put(audio_url, data=f)
+                if response.status_code == 200:
+                    print("Audio uploaded successfully.")
+                else:
+                    print(f"Failed to upload audio. Status Code: {response.status_code}, Response: {response.text}")
+        except Exception as e:
+            print(f"Error uploading audio: {str(e)}")
 
     def onRecordingStarted(self):
         self.recordButton.setIcon(QIcon(resource_path('../icons/record-stop.svg')))
